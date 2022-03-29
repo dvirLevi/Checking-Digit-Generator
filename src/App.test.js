630a939e8094e6982bd3f51215d5fb5d8b71646c
dvirLevi <dvirleviapp@gmail.com>
2022-03-29 17:59:28 +0300

    add

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Checking Digit Generator for Israeli Id/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("change values via the fireEvent.change method", () => {
+  const handleChange = jest.fn();
+  const { container } = render(<input type="text" onChange={handleChange} />);
+  const input = container.firstChild;
+  fireEvent.change(input, { target: { value: "a" } });
+  expect(handleChange).toHaveBeenCalledTimes(1);
+  expect(input.value).toBe("a");
 });
